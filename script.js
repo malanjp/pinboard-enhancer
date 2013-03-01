e58c64e6114defb7bb04b4fe5b48ce8797667cae
mshibata <shibata.michihiro@gmail.com>
2013-03-01 20:55:36 +0900

    improved quick copy animation

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 $(function(){
   $('.bookmark').each(function(){
-    html = '<span><a href="#" class="quick_copy" onclick="return false">quick copy</a></span><span style="display:none;">copied!</span>';
+    var html = '&nbsp;<span><a href="#" class="quick_copy" onclick="return false">quick copy</a></span><span style="display:none;">copied!</span>';
     $(this).find('.edit_links').append(html);
   });
 
   $('.quick_copy').bind('click', function(){
-    quick_copy = $(this);
-    bookmark_link = $(this).parents('div.display').find('a.bookmark_title');
-    url = bookmark_link.attr('href');
-    title = bookmark_link.text();
+    var quick_copy = $(this);
+    var bookmark_link = $(this).parents('div.display').find('a.bookmark_title');
+    var url = bookmark_link.attr('href');
+    var title = bookmark_link.text();
+
+    blink = setInterval(function() {
+      quick_copy.is(":hidden") ? quick_copy.fadeIn() : quick_copy.fadeOut();
+    }, 500);
 
     $.ajax({
       url: 'https://api.pinboard.in/v1/posts/add',
       data: {'url':url, 'description':title},
       dataType: 'xml',
       success: function(data){
-          console.log($(data).find('result').attr('code'));
           if ($(data).find('result').attr('code') == 'done') {
             $(quick_copy).parents('div.display').find('span').toggle('slow');
           }
+          clearInterval(blink);
       }
     });
   });
