a6e1c50eeaa54680389d68747849348788d14190
mshibata <shibata.michihiro@gmail.com>
2013-03-01 22:23:18 +0900

    added feature "keybind"

---
+++ keybind.js
@@ -0,0 +1,44 @@
+$(function(){
+  var bookmark_titles = $('.bookmark_title');
+  var current_key = 0;
+  var cursor = undefined;
+  var max = bookmark_titles.length - 1;
+  $(document).keypress(function (e) {
+    console.log(e.which);
+    switch (e.which) {
+      case 106: // j key
+        if (cursor != undefined) {
+          cursor++;
+          if (cursor > max) {
+            cursor = max;
+          }
+        } else {
+          cursor = 0;
+        }
+        bookmark_titles[cursor].focus();
+        current_key = 106;
+        break;
+
+      case 107: // k key
+        cursor--;
+        if (cursor < 0) {
+          cursor = 0;
+        }
+        bookmark_titles[cursor].focus();
+        current_key = 107;
+        break;
+
+      case 111: // o key
+        window.open($(':focus').attr('href'), '_blank');
+        break;
+
+      case 113: // q key
+        console.log($(':focus').parent().find('.quick_copy'));
+        $(':focus').parent().find('.quick_copy').click();
+        break;
+
+      default:
+        break
+    }
+  });
+});
